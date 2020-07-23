eefb5b86f3d0a972b2be536cad1cf7640362a4a3
jhonatan-JS <jhonatanribeiro.mei@hotmail.com>
2020-07-22 20:50:21 -0400

    Styling details

--- Frontend/src/pages/Repository/index.tsx
+++ Frontend/src/pages/Repository/index.tsx
@@ -1,7 +1,67 @@
 import React from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronsLeft, FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
+
+interface RepositoryParams {
+    repository: string;
+}
 
 const Repository: React.FC = () => {
-    return <h1>Repository</h1>;
+    // const { params } = useRouteMatch<RepositoryParams>();
+
+    return (
+        <>
+            <Header>
+                <img src={logoImg} alt="Github Explorer" />
+                <Link to="/">
+                    <FiChevronsLeft size={16} />
+                    voltar
+                </Link>
+            </Header>
+
+            <RepositoryInfo>
+                <header>
+                    <img
+                        src="https://avatars0.githubusercontent.com/u/28929274?v=4"
+                        alt="Rocketseat"
+                    />
+                    <div>
+                        <strong>rocketseat/unform</strong>
+                        <p>descrioção do repositório</p>
+                    </div>
+                </header>
+                <ul>
+                    <li>
+                        <strong>1808</strong>
+                        <span>Start</span>
+                    </li>
+                    <li>
+                        <strong>48</strong>
+                        <span>Forks</span>
+                    </li>
+                    <li>
+                        <strong>67</strong>
+                        <span>Issues abertas</span>
+                    </li>
+                </ul>
+            </RepositoryInfo>
+
+            <Issues>
+                <Link to="oaoskaoks">
+                    <div>
+                        <strong>aoskaosk</strong>
+                        <p>aoskaoskaosk</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </Link>
+            </Issues>
+        </>
+    );
 };
 
 export default Repository;
